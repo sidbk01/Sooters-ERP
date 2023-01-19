7fda1c7db925c90b30e7808c75593dab2a4e2235
Lance Hart <shipsimfan@hotmail.com>
2023-01-19 11:41:21 -0500

    Updated create release script

--- create_release.ps1
+++ create_release.ps1
@@ -2,9 +2,10 @@ Write-Output "Building . . ."
 cargo build --release
 
 Write-Output "Copying required files"
-Copy-Item -Path .\css -Destination .\release\css -Recurse
-Copy-Item -Path .\js -Destination .\release\js -Recurse
-Copy-Item -Path .\templates -Destination .\release\templates -Recurse
+Copy-Item -Path .\css -Destination .\release\css -Recurse -Force
+Copy-Item -Path .\js -Destination .\release\js -Recurse -Force
+Copy-Item -Path .\images -Destination .\release\images -Recurse -Force
+Copy-Item -Path .\templates -Destination .\release\templates -Recurse -Force
 Copy-Item -Path .\target\release\sooters_erp.exe -Destination .\release\sooters_erp.exe
 
 Write-Output "Packaging archive . . ."
