c66e63f49c2e4e7d03df3a3325b10375b4d0eed6
Siddardha Mayakuntla <56807006+sidbk01@users.noreply.github.com>
2024-01-26 13:47:24 -0500

    Added APS film type

--- ts/model/order_types/film_roll.ts
+++ ts/model/order_types/film_roll.ts
@@ -4,6 +4,7 @@ export enum FilmType {
     Color = 1,
     BlackWhite,
     _120mm,
+    APS,
 }
 
 export enum Exposures {
@@ -42,6 +43,7 @@ export class FilmTypeOption implements SelectOption {
             new FilmTypeOption(FilmType.Color),
             new FilmTypeOption(FilmType.BlackWhite),
             new FilmTypeOption(FilmType._120mm),
+            new FilmTypeOption(FilmType.APS),
         ];
     }
 
